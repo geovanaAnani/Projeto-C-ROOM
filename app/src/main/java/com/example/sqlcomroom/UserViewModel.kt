bdb991188f2f0a815c737d5d415fd68973847de9
geovanaAnani <geovana.ananias04@gmail.com>
2022-04-01 16:37:27 -0300

    APP com ROOM

---
+++ app/src/main/java/com/example/sqlcomroom/UserViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.sqlcomroom
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.sqlcomroom.model.User
+import com.example.sqlcomroom.model.UserDataBase
+import com.example.sqlcomroom.model.UserRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class UserViewModel (context: Context?): ViewModel() {
+
+    val lerTodosOsDados: LiveData<List<User>>
+    private val repository: UserRepository
+
+    init {
+        val userDao = UserDataBase.getDatabase(context!!).userDao()
+        repository = UserRepository(userDao)
+        lerTodosOsDados = repository.lerTodosOsDados
+    }
+
+    fun addUser(user: User){
+        viewModelScope.launch (Dispatchers.IO){
+            repository.addUser(user)
+        }
+    }
+}
